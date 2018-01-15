2df40a50f532003372f3ddc34f7117a4a5135427
Jacob1943 <24765535+Jacob1943@users.noreply.github.com>
2018-01-15 16:26:49 +0800

    add hystrix for ribbon

--- ribbon-demo/src/main/java/me/ye/ribbondemo/controller/Controller.java
+++ ribbon-demo/src/main/java/me/ye/ribbondemo/controller/Controller.java
@@ -1,5 +1,6 @@
 package me.ye.ribbondemo.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,9 +18,16 @@ public class Controller {
     @Autowired
     private RestTemplate restTemplate;
 
+
+    @HystrixCommand(fallbackMethod = "errorFallback")
     @GetMapping("/hello")
     public String hello() {
 
         return this.restTemplate.getForObject("http://eureka-client-demo/hello", String.class);
+    }
+
+
+    public String errorFallback() {
+        return "Error!";
     }
 }
